f97234c7610a71b7fd9581084f2f04f0729294e4
max7394 <mlysenko92@gmail.com>
2022-05-23 09:46:13 +0300

    feat: add solve to 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+const oldFolder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy'); 
+
+fs.promises.mkdir(newFolder, {recursive: true}, () => {});
+fs.readdir(newFolder,{withFileTypes: true}, (err, items) => {
+  items.forEach((item) => {fs.unlink(path.join(newFolder, item.name), () => {});
+  });
+  fs.readdir(oldFolder,{withFileTypes: true}, (err, items) => {
+    items.forEach((item) => {
+      fs.copyFile(path.join(oldFolder, item.name), path.join(newFolder, item.name),() => {});});
+  });
+});
